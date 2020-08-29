565aef6a86b81dc52457bb3330b4f162f5dc8ca7
User-Pavel <pavelskalozub@gmail.com>
2020-08-29 15:09:45 +0300

    Version of HomeWork - 1

---
+++ Exercise_4_1.java
@@ -0,0 +1,20 @@
+package HomeWork_1;
+
+import java.util.Scanner;
+
+public class Exercise_4_1 {
+    public static void main(String[] args) {
+        sleepIn(true,false);
+    }
+    static boolean sleepIn(boolean isWeekday, boolean isVacation) {
+        if (isVacation == true || isWeekday == false) {
+            System.out.println("Можно спать");
+            return true;
+        } else {
+            System.out.println("Надо на работу");
+            return false;
+        }
+
+// boolean result = sleepIn(...)е
+    }
+}
